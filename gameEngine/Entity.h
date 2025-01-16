618654de603db386cb7e1858c45ffdfc70f05116
Jack <jacklaing1234@gmail.com>
2025-01-16 22:15:15 +0000

    Added Entity.h and Entity.cpp

---
+++ gameEngine/Entity.h
@@ -0,0 +1,15 @@
+#ifndef MYCLASS_H  // Include guard to prevent multiple inclusions
+#define MYCLASS_H
+
+#include <iostream>  // Include necessary libraries
+
+// Class declaration
+class MyClass {
+public:
+    
+
+private:
+    
+};
+
+#endif // MYCLASS_H
