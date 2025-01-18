5fed00ae6e3d3a66496e7988d6071726c4d2c783
Jack <jacklaing1234@gmail.com>
2025-01-18 17:19:10 +0000

    Added AddComponent to EntityManager.cpp

--- gameEngine/EntityManager.cpp
+++ gameEngine/EntityManager.cpp
@@ -2,5 +2,8 @@
 
 template <typename T, typename... Args>
 void EntityManager::addComponent(Entity entity, Args... args) {
+	int entityID = entity.getId();
 
+	// Accesses the std::unordered_map for a given entity and creates and stores a unique pointer to its component
+	entities[entityID][std::type_index(typeid(T))] = std::make_unique<T>(std::forward<Args>(args)...);
 }
