465e76e262790540255f4a36a6ae8cd620ac873b
Jack <jacklaing1234@gmail.com>
2025-01-18 16:46:30 +0000

    Added Component.h and Component.cpp to use in EntityManager

---
+++ gameEngine/Component.h
@@ -0,0 +1,16 @@
+#ifndef Component_H 
+#define Component_H
+
+#include <iostream> 
+
+// Class declaration
+class Component {
+private:
+
+
+public:
+
+
+};
+
+#endif 
--- gameEngine/EntityManager.h
+++ gameEngine/EntityManager.h
@@ -1,12 +1,16 @@
 #ifndef EntityManager_H 
 #define EntityManager_H
 
+#include <unordered_map>
+#include <typeindex>
+#include <memory>
+#include "Component.h"
 #include <iostream> 
 
 // Class declaration
 class EntityManager {
 private:
-    
+    std::unordered_map<int, std::unordered_map < std::type_index, std::unique_ptr<Component>>> entities;
 
 public:
 
