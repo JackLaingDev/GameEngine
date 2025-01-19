3ad86838e860071dbcb50718abb5bc19f347c801
Jack <jacklaing1234@gmail.com>
2025-01-19 18:16:48 +0000

    Refactored InputManager to remove if statements

--- gameEngine/InputManager.cpp
+++ gameEngine/InputManager.cpp
@@ -7,7 +7,14 @@ InputManager::InputManager(sf::Window* win, EventManager* em) : win(win), em(em)
 
 void InputManager::update() {
     
-    std::optional<sf::Event> eventSF; // SFML Event
+    // Keys to check
+    const std::vector<sf::Keyboard::Key> keysToCheck = {
+    sf::Keyboard::Key::W,
+    sf::Keyboard::Key::A,
+    sf::Keyboard::Key::S,
+    sf::Keyboard::Key::D,
+    sf::Keyboard::Key::Space
+    };
 
      // event type will always be keypress for input manager as its only looking for keypreses
 
@@ -17,65 +24,17 @@ void InputManager::update() {
         // Check for Keypress
         if (eventSF->is<sf::Event::KeyPressed>()) {
 
-            // Create game engine Event for W press
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-                
-                // Create Custom event
-                Event event; 
-                keyPressData kpData = { sf::Keyboard::Key::W };
+            for (const auto& key : keysToCheck)
+            {
+                if (sf::Keyboard::isKeyPressed(key))
+                {
+                    // Create and publish custom event
+                    Event event;
+                    event.type = eventType::keyPress;
+                    event.data = keyPressData{ key };
 
-                // Set event attributes
-                event.type = eventType::keyPress;
-                event.data = {kpData};
-            }
-
-            // Create game engine Event for A press
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-
-                // Create Custom event
-                Event event;
-                keyPressData kpData = { sf::Keyboard::Key::A };
-
-                // Set event attributes
-                event.type = eventType::keyPress;
-                event.data = { kpData };
-            }
-
-            // Create game engine Event for S press
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-
-                // Create Custom event
-                Event event;
-                keyPressData kpData = { sf::Keyboard::Key::S };
-
-                // Set event attributes
-                event.type = eventType::keyPress;
-                event.data = { kpData };
-            }
-
-            // Create game engine Event for D press
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-
-                // Create Custom event
-                Event event;
-                keyPressData kpData = { sf::Keyboard::Key::D };
-
-                // Set event attributes
-                event.type = eventType::keyPress;
-                event.data = { kpData };
-            }
-
-            // Create game engine Event for Space press
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)) {
-
-                // Create Custom event
-                Event event;
-                keyPressData kpData = { sf::Keyboard::Key::Space };
-
-                // Set event attributes
-                event.type = eventType::keyPress;
-                event.data = { kpData };
-                em->publish
+                    em->publish(event); // Publish the event to the EventManager
+                }
             }
         }
     }
