20c5702d83c79d71f570c377e1cd3844c0b57d89
Jack <jacklaing1234@gmail.com>
2025-01-19 16:00:27 +0000

    Fixed issue with template definitions not being in header files

--- gameEngine/GameLoop.cpp
+++ gameEngine/GameLoop.cpp
@@ -7,9 +7,16 @@ void GameLoop::run()
 {
 	this->isRunning = true;
 
+	Entity entity(1);
+	em->addComponent<HealthComponent>(entity, 100);
+
 	while (isRunning) {
 
 		// Game Loop logic goes here
+		// take input
+		// update systems
+		// process events
+		// render
 
 	}
 }
