12dbbfb449652702f16c5abc9a9abf1563ddfca4
Jack <jacklaing1234@gmail.com>
2025-01-25 15:46:32 +0000

    Movementmanager now moves players

--- gameEngine/MovementManager.cpp
+++ gameEngine/MovementManager.cpp
@@ -6,29 +6,32 @@ MovementManager::MovementManager(EntityManager* entityManager) : entityManager(e
 
 void MovementManager::move(const Event& event)
 {
-	Entity entity(1);
-	auto velocityComponent = entityManager->getComponent<VelocityComponent>(entity);
-	auto transformComponent = entityManager->getComponent<TransformComponent>(entity);
+	const auto& entitiesToMove = entityManager->getEntitiesByComponent<PlayerComponent>();
 
-	if (event.type == eventType::keyPress) {
+	for (const auto& entity : entitiesToMove) {
+		auto velocityComponent = entityManager->getComponent<VelocityComponent>(entity);
+		auto transformComponent = entityManager->getComponent<TransformComponent>(entity);
 
-		auto eventData = std::get<keyPressData>(event.data);
-		auto currentPos = transformComponent->position;
+		if (event.type == eventType::keyPress) {
 
-		switch (eventData.key)
-		{
-		case sf::Keyboard::Key::W:
-			transformComponent->position = sf::Vector2f(currentPos.x, currentPos.y - velocityComponent->velocity.y); 
-			break;
-		case sf::Keyboard::Key::A:
-			transformComponent->position = sf::Vector2f(currentPos.x - velocityComponent->velocity.x, currentPos.y);
-			break;
-		case sf::Keyboard::Key::S:
-			transformComponent->position = sf::Vector2f(currentPos.x, currentPos.y + velocityComponent->velocity.y);
-			break;
-		case sf::Keyboard::Key::D:
-			transformComponent->position = sf::Vector2f(currentPos.x + velocityComponent->velocity.x, currentPos.y);
-			break;
+			auto eventData = std::get<keyPressData>(event.data);
+			auto currentPos = transformComponent->position;
+
+			switch (eventData.key)
+			{
+			case sf::Keyboard::Key::W:
+				transformComponent->position = sf::Vector2f(currentPos.x, currentPos.y - velocityComponent->velocity.y);
+				break;
+			case sf::Keyboard::Key::A:
+				transformComponent->position = sf::Vector2f(currentPos.x - velocityComponent->velocity.x, currentPos.y);
+				break;
+			case sf::Keyboard::Key::S:
+				transformComponent->position = sf::Vector2f(currentPos.x, currentPos.y + velocityComponent->velocity.y);
+				break;
+			case sf::Keyboard::Key::D:
+				transformComponent->position = sf::Vector2f(currentPos.x + velocityComponent->velocity.x, currentPos.y);
+				break;
+			}
 		}
 	}
 }
