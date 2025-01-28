67edf9ac111b3c19fbb14dd300dbadbdcfed6532
Jack <jacklaing1234@gmail.com>
2025-01-28 18:56:47 +0000

    Changed to region class

--- gameEngine/TerrainManager.cpp
+++ gameEngine/TerrainManager.cpp
@@ -1,1 +1,17 @@
 #include "TerrainManager.h"
+
+void TerrainManager::addRegion(sf::Vector2f pos, sf::Vector2f size)
+{
+}
+
+region::region(sf::Vector2f pos, sf::Vector2f size, sf::Color colour) : pos(pos), size(size), colour(colour)
+{
+	rect.setSize(size);
+	rect.setPosition(pos);
+	rect.setFillColor(colour);
+}
+
+sf::RectangleShape region::getRect()
+{
+	return rect;
+}
--- gameEngine/TerrainManager.h
+++ gameEngine/TerrainManager.h
@@ -3,21 +3,33 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 
 class TerrainManager {
 private:
 
+	std::vector<region> regions;
 
 public:
 
+	void addRegion(sf::Vector2f pos, sf::Vector2f size);
+	std::vector<region> getRegions();
+
 };
 
-struct region {
-
-	sf::RectangleShape rect;
+class region {
+private:
 	sf::Vector2f pos;
 	sf::Vector2f size;
+	sf::Color colour;
+	sf::RectangleShape rect;
+
+public:
+
+	region(sf::Vector2f pos, sf::Vector2f size, sf::Color colour);
+
+	sf::RectangleShape getRect();
 
 };
 
