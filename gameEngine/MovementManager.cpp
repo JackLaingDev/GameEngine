e742af07235a33a0d762c4a9abe0a1886b6cf0d2
Jack <jacklaing1234@gmail.com>
2025-03-23 13:30:14 +0000

    MovementManager now updates all velocity entities

--- gameEngine/MovementManager.cpp
+++ gameEngine/MovementManager.cpp
@@ -8,27 +8,28 @@ MovementManager::MovementManager(EntityManager* entityManager) : entityManager(e
 // Collider componenet should derive its position from the transofmr componenet within the collision system not here
 void MovementManager::update(float deltatime)
 {
-	auto entities = entityManager->getEntitiesByComponent<PlayerComponent>();
-	auto player = entities[0];
+	auto entities = entityManager->getEntitiesByComponent<VelocityComponent>();
 
-	auto velocityComponent = entityManager->getComponent<VelocityComponent>(player);
-	auto transformComponent = entityManager->getComponent<TransformComponent>(player);
+	for (auto entity : entities) {
+		auto velocityComponent = entityManager->getComponent<VelocityComponent>(entity);
+		auto transformComponent = entityManager->getComponent<TransformComponent>(entity);
 
-	// Clamp x velocity
-	velocityComponent->velocity.x = std::clamp(
-		velocityComponent->velocity.x,
-		velocityComponent->minVelocity.x,
-		velocityComponent->maxVelocity.x
-	);
+		// Clamp x velocity
+		velocityComponent->velocity.x = std::clamp(
+			velocityComponent->velocity.x,
+			velocityComponent->minVelocity.x,
+			velocityComponent->maxVelocity.x
+		);
 
-	// Clamp y velocity
-	velocityComponent->velocity.y = std::clamp(
-		velocityComponent->velocity.y,
-		velocityComponent->minVelocity.y,
-		velocityComponent->maxVelocity.y
-	);
+		// Clamp y velocity
+		velocityComponent->velocity.y = std::clamp(
+			velocityComponent->velocity.y,
+			velocityComponent->minVelocity.y,
+			velocityComponent->maxVelocity.y
+		);
 
-	transformComponent->position.x += deltatime * velocityComponent->velocity.x;
-	transformComponent->position.y += deltatime * velocityComponent->velocity.y;
+		transformComponent->position.x += deltatime * velocityComponent->velocity.x;
+		transformComponent->position.y += deltatime * velocityComponent->velocity.y;
+	}
 	
 }
