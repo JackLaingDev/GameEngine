92d351a9e221f001bc1acb901871203725dbc6fd
Jack <jacklaing1234@gmail.com>
2025-03-25 18:39:47 +0000

    Updated EventManager to allow subscription to SFML events using std::type_index.

--- gameEngine/EventManager.cpp
+++ gameEngine/EventManager.cpp
@@ -1,61 +1,45 @@
 #include "EventManager.h"
 
-EventManager::EventManager(sf::Window* win) : win(win)
-{
-}
+EventManager::EventManager(sf::Window* win) : win(win) {}
 
-void EventManager::enqueue(const Event& customEvent)
-{
+void EventManager::enqueue(const Event& customEvent) {
     customEvents.push_back(customEvent);
 }
 
-void EventManager::enqueue(const sf::Event& sfmlEvent)
-{
-    SFMLEvents.push_back(sfmlEvent);
+void EventManager::enqueue(const sf::Event& sfmlEvent) {
+    sfmlEvents.push_back(sfmlEvent);
 }
 
-void EventManager::pollSFMLEvents()
-{
+void EventManager::pollSFMLEvents() {
     while (const std::optional eventSF = win->pollEvent()) {
-        SFMLEvents.push_back(eventSF);
+        enqueue(*eventSF);
     }
 }
 
-/*
-Subscribe functions to events in gameloop, when an event is published it will call the corresponding function.
-*/
 void EventManager::subscribe(eventType type, std::function<void(const Event&)> callback) {
-    // Add the callback to the list of subscribers for the given event type
     customSubscribers[type].push_back(callback);
 }
 
-void EventManager::subscribe(sf::Event type, std::function<void(const sf::Event&)> callback)
-{
-    SFMLSubscribers[type].push_back(callback);
-}
-
-/*
-Iterates through both custom and SFML event queues, calls their corresponding functions
-*/
 void EventManager::publish() {
-
     for (auto& event : customEvents) {
-        // Check if there are subscribers for the event's type
         if (customSubscribers.find(event.type) != customSubscribers.end()) {
-            // Invoke each callback with the event
             for (auto& callback : customSubscribers[event.type]) {
                 callback(event);
             }
         }
     }
-    for (auto& event : SFMLEvents) {
-        // Check if there are subscribers for the event's type
-        if (SFMLSubscribers.find(event) != SFMLSubscribers.end()) {
-            // Invoke each callback with the event
-            for (auto& callback : SFMLSubscribers[event]) {
-                callback(event);
+
+    for (const auto& event : sfmlEvents) {
+        event.visit([&](const auto& concreteEvent) {
+            auto it = sfmlSubscribers.find(std::type_index(typeid(concreteEvent)));
+            if (it != sfmlSubscribers.end()) {
+                for (auto& callback : it->second) {
+                    callback(event);
+                }
             }
-        }
+            });
     }
 
+    customEvents.clear();
+    sfmlEvents.clear();
 }
--- gameEngine/EventManager.h
+++ gameEngine/EventManager.h
@@ -1,40 +1,45 @@
 #ifndef EventManager_H 
 #define EventManager_H
 
-#include <iostream> 
+#include <unordered_map>
+#include <vector>
 #include <functional>
-
+#include <typeindex>
+#include <SFML/Window.hpp>
 #include "Event.h"
-
 
 class EventManager {
 private:
-    // Map event types to a list of callbacks (subscribers)
-    std::unordered_map<eventType, std::vector<std::function<void(const Event&)>>> customSubscribers;
-    std::unordered_map<sf::Event, std::vector<std::function<void(const sf::Event&)>>> SFMLSubscribers;
     sf::Window* win;
 
-    // Event queues
+    std::unordered_map<eventType, std::vector<std::function<void(const Event&)>>> customSubscribers;
+    std::unordered_map<std::type_index, std::vector<std::function<void(const sf::Event&)>>> sfmlSubscribers;
+
     std::vector<Event> customEvents;
-    std::vector<sf::Event> SFMLEvents;
+    std::vector<sf::Event> sfmlEvents;
 
 public:
-
     EventManager(sf::Window* win);
 
     void enqueue(const Event& customEvent);
     void enqueue(const sf::Event& sfmlEvent);
 
-    // Func to poll SFML events
     void pollSFMLEvents();
 
-    // Subscribe a callback to an event type
     void subscribe(eventType type, std::function<void(const Event&)> callback);
-    void subscribe(sf::Event type, std::function<void(const sf::Event&)> callback);
 
-    // Publish an event, invoking all callbacks for its type
+    template<typename TEventSubtype>
+    void subscribe(std::function<void(const TEventSubtype&)> callback) {
+        sfmlSubscribers[std::type_index(typeid(TEventSubtype))].push_back(
+            [callback](const sf::Event& event) {
+                if (const auto* concreteEvent = event.getIf<TEventSubtype>()) {
+                    callback(*concreteEvent);
+                }
+            }
+        );
+    }
+
     void publish();
 };
 
-
-#endif 
+#endif
